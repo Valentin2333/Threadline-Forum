e00d72a851d0ee5f753043f1897a596ea343d962
Stanimir <shpeev0@gmail.com>
2026-01-31 23:49:44 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useEffect, useState } from 'react';
+// import './App.css'
+// import { supabase } from './api/supabaseClient';
+import Register from './components/Register';
+
+
+function App() {
+
+  return (
+    <Register/>
+  )
+}
+
+export default App
